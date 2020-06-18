f3c0ec38228fba27475e93d5c030ea2863850059
Onifs10 <onifadebolu64@gmail.com>
2020-06-18 18:00:17 +0100

    5

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $dotenv->load();
 
 //die(var_dump($_SERVER));
 $wallet = new Wallet($_ENV['Secret_Key']);
-$wallet->Details('MTN',2000,'08033377992,');
+$wallet->connect()->Details('MTN',2000,'08033377992,');
 try{
     $output = $wallet->send();
     var_dump($output);
